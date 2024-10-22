145016f3ffdc243620021739f30f775601c47ef0
stamatiapapazis <stamatiaapapazis@gmail.com>
2024-10-21 21:56:40 -0400

    Revert "fixing sweatshirt colors"
    This reverts commit 2e4fe379eee9eeba0d483f8d3be3f2811f0fc80f.

--- src/main.js
+++ src/main.js
@@ -109,7 +109,7 @@ import {
 
         let currentFacingMode = 'user';
         let currentCameraType = 'front';
-        let currentLens = 0;
+        let currentLens = 2;
 
         const startCamera = async (facingMode, cameraType) => {
             let mediaStream = await navigator.mediaDevices.getUserMedia({
@@ -143,18 +143,18 @@ import {
         });
 
         document.getElementById('RedSweatshirt').addEventListener('click', () => {
-            session.applyLens(lenses[0]);
-            currentLens = 0;
-        });
-
-        document.getElementById('GreySweatshirt').addEventListener('click', () => {
             session.applyLens(lenses[2]);
             currentLens = 2;
         });
 
-        document.getElementById('WhiteSweatshirt').addEventListener('click', () => {
+        document.getElementById('GreySweatshirt').addEventListener('click', () => {
             session.applyLens(lenses[1]);
             currentLens = 1;
+        });
+
+        document.getElementById('WhiteSweatshirt').addEventListener('click', () => {
+            session.applyLens(lenses[0]);
+            currentLens = 0;
         });
     }
 
